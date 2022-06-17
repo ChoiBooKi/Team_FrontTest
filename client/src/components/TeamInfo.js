21dc93abbbb93eabfbe4e7ab4d9fd526e2f598a6
BooKi <qnrl9034@naver.com>
2022-06-17 15:27:06 +0900

    리스트, 팀정보 css 수정

--- client/src/components/TeamInfo.js
+++ client/src/components/TeamInfo.js
@@ -85,13 +85,13 @@ function TeamInfo () {
         {Status ? "편집" : "편집 완료"}
       </button>
       {/* <h1>{TeamInfo && TeamInfo[0].teamname}</h1> */}
-      {Status ? <p className="teamname">{TeamName && TeamName}</p> : <input disabled={Status} onChange={onTeamnameHandler} value={TeamName && TeamName} className="teamname1"></input>}
+      {Status ? <h3 className="teamname">{TeamName && TeamName}</h3> : <input disabled={Status} onChange={onTeamnameHandler} value={TeamName && TeamName} className="teamname1"></input>}
       {/* <input disabled={Status} value={TeamInfo && TeamInfo[0].teamname} className="teamname"></input> */}
       <Avatar 
         className='avatar'
         alt="팀이미지" 
         src={Image} 
-        sx={{ width: 200, height: 200 }} 
+        sx={{ width: '15vh', height: '15vh', marginTop:'-1.5vh', marginBottom:'-1.5vh' }} 
         onClick={()=>{fileInput.current.click()}}/>
       <input 
         className='input'
@@ -102,40 +102,40 @@ function TeamInfo () {
         onChange={onChange}
         ref={fileInput}/>
       <div className='teaminfo'>
-        <h2 style={{marginRight:"50%", marginTop:"10%"}}>팀 소개</h2>
+        <h3 style={{marginRight:"78%", marginTop:"10%"}}>팀 소개</h3>
         <textarea 
           maxLength="200" 
           value={Intro && Intro} //벨류라서 변경이 안됨
           onChange={onIntroHandler}
           disabled={Status}
-          style={{resize: "none", width: "350px", border: "2px solid black",   borderRadius:"10px"}}
-          rows="6">
+          style={{resize: "none", width: "350px", border: "2px solid black",   borderRadius:"10px", width: "26vh"}}
+          rows="4">
           {/* {TeamInfo && TeamInfo.intro} */}
         </textarea>
         <div style={{display:"flex"}}>
-          <h2 style={{margin:"5%", marginLeft:"17%"}}>팀장</h2>
+          <h3 style={{marginTop:"5%"}}>팀장</h3>
           <div style={{marginTop:"5.5%"}}>{TeamInfo && TeamInfo[0].leader}</div>
         </div>
         <div style={{display:"flex"}}>
-          <h2 style={{margin:"5%", marginLeft:"17%"}}>팀 등록일</h2>
+          <h3 style={{marginTop:"5%"}}>팀 등록일</h3>
           <p style={{marginTop:"5.5%"}}>{TeamInfo && TeamInfo[0].date}</p>
         </div>
         <div style={{display:"flex"}}>
-          <h2 style={{margin:"5%", marginLeft:"17%"}}>총 선수 수</h2>
+          <h3 style={{marginTop:"5%"}}>총 선수 수</h3>
           <p style={{marginTop:"5.5%"}}>{TeamInfo && TeamInfo[0].numofplayer}</p>
         </div>
         <div style={{display:"flex"}}>
-          <h2 style={{margin:"5%", marginLeft:"17%"}}>연령대</h2>
+          <h3 style={{marginTop:"5%"}}>연령대</h3>
           <p style={{marginTop:"5.5%"}}>{TeamInfo && TeamInfo[0].age}</p>
         </div>
         {Status ? 
           <div style={{display:"flex"}}>
-            <h2 style={{margin:"5%", marginLeft:"17%"}}>활동지역</h2>
+            <h3 style={{marginTop:"5%"}}>활동지역</h3>
             <p style={{marginTop:"5.5%"}}>{TeamInfo && TeamInfo[0].region}</p>
           </div>
         : 
           <div style={{display:"flex"}}>
-            <h2 style={{margin:"5%", marginLeft:"17%"}}>활동지역</h2>
+            <h3 style={{marginTop:"5%", marginRight: "20px"}}>활동지역</h3>
             <select onChange={onRegionHandler} value={Region} style={{width:"100px", height:"30px"}}>
               {regionList.map((item, id) => (
                 <option value={item} key={id}>
